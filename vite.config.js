4ff1509884e6632e01b9e32013970a52aea493ee
Kaleb-Espinel <kalebespinel.09@outlook.es>
2025-06-28 11:07:45 -0500

    v.2.6.9.1

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/vite-app-web/',
+  base: "https://kaleb-espinel.github.io/vite-app-web/",
   plugins: [react(), tailwindcss()],
 })
